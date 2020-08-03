15fd49e70a902c8d3ac2d8b2d73e24aa77d5904f
satcom886 <rostik.medved@gmail.com>
2020-08-03 13:42:54 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # pycommander
 A tool to make my life better...
+
+[![CodeFactor](https://www.codefactor.io/repository/github/satcom886/pycommander/badge)](https://www.codefactor.io/repository/github/satcom886/pycommander)
+
+# Requirements
+ * Python (3.5+)
+ * `ssh`
+ * `wol` (if wake is enabled)
+ * python-yaml (PyYAML)
+
+# Usage
+When the program is started, the config is read and the hosts are processed top to bottom.
+
+# Configuration
+The config file has a very simple structure. Below is a sample config with comments.
+```yaml
+clients:
+ Tellurium: # Name of the computer - can be anything, doesn't need to match hostname
+  ip: Tellurium.local # IP address or hostname used to connect to the client.
+  mac: de:ad:be:ef:00:00 # MAC address. Only used for waking up the client. Doesn't need to be specified when 'wake' is 'no'
+  wake: yes # yes/no. Says if the client should be woken up using Wake on LAN.
+  username: satcom886 # Username used to connect with SSH.
+ Argon:
+  ip: Argon.local
+  mac: 00:00:00:00:00:00
+  wake: yes
+  username: satcom886
+
+command: "echo \"Hello from the other side!\" && sleep 5 && sudo systemctl poweroff" # Command that will be executed on all clients.
+```
---
+++ pycommander.py
@@ -0,0 +1,48 @@
+import os
+import subprocess
+import yaml
+
+config = yaml.safe_load(open(os.path.dirname(os.path.abspath(__file__)) + "/config.yml"))
+
+def main():
+    for client in config["clients"]:
+        if config["clients"][client]["wake"] is True:
+            wake_computer(config["clients"][client]["mac"], client)
+        wait_for_ping(config["clients"][client]["ip"], client)
+        start_command(config["command"], \
+            config["clients"][client]["username"], \
+            config["clients"][client]["ip"], client)
+
+def wake_computer(mac, name):
+    print("[", name, "]", " Waking up ", "(MAC: ", mac, ").", sep="")
+    try:
+        subprocess.run(["wol", mac], check=True, capture_output=True)
+    except subprocess.CalledProcessError:
+        print("[", name, "]", " wol returned a non-zero exit code,", sep="")
+        exit(1)
+
+def wait_for_ping(ip, name):
+    print("[", name, "]", " Waiting for ", ip, " to come online.", sep="")
+    fail_counter = 0
+    while True:
+        if fail_counter >= 15:
+            break
+            print("[", name, "]", "The client took too long to start, skipping.")
+        else:
+            try:
+                subprocess.run(["ping", ip, "-c 3"], check=True, capture_output=True)
+            except subprocess.CalledProcessError:
+                print("[", name, "]", " Attempt ", str(fail_counter + 1), "/15.", " Still down...", sep="")
+                fail_counter += 1
+            else:
+                print("[", name, "]", " And we are up! This took ", str(fail_counter + 1), " attempts.", sep="")
+                break
+
+def start_command(command, username, ip, name):
+    uname_ip = username + "@" + ip
+    print("[", name, "]", " Executing \'", command, "\' on ", uname_ip, sep="")
+    subprocess.run(["ssh", uname_ip, command])
+    print("[", name, "]", " Execution finished!", sep="")
+
+if __name__ == "__main__":
+    main()
